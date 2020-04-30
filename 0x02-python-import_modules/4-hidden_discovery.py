d06beca3ede6d16b7a85cd4d2ec4eae24f7b918a
Duvan <duvanjarin@gmail.com>
2020-04-30 16:40:14 -0500

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__ == "__name__":
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print("{}".format(i))
+
+if __name__ == "__main__":
+    for str in dir(hidden_4):
+        if str[:2] != "__":
+            print("{}".format(str))
