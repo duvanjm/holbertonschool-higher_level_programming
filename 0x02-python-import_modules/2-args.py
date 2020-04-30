c2fa3698eabcfef780b1024d45039ca7a42b34fe
Duvan <duvanjarin@gmail.com>
2020-04-30 15:53:59 -0500

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = len(argv) -1
+    if i == 0:
+        print("{}".format("0 arguments."))
+    elif i == 1:
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} {}".format(i, "arguments:"))
+        for a in range(1, i +1):
+            print("{:d}: {}".format(i, argv[i]))
+                   
