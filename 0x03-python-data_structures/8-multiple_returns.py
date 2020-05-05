473eb5e06d47086d7ac3b142546ec2645a73cf08
Duvan <duvanjarin@gmail.com>
2020-05-05 18:28:04 -0500

    8-main.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence is "" or not sentence:
         return (0, None)
     return (len(sentence), sentence[0])
