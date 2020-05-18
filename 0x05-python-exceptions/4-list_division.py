160fdd345843b5a2162c23cff924eccdb0326102
duvanjm <duvanjarin@gmail.com>
2020-05-18 18:10:49 -0500

    fix 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     for i in range(list_length):
         try:
-            new.append(my_list_1 / my_list_2)
+            new.append(my_list_1[i] / my_list_2[i])
             error = ""
         except TypeError:
             new.append(0)
