c37e62cca13d38ba792c6d2b1d4002eff8a91cf6
duvanjm <duvanjarin@gmail.com>
2020-05-19 19:00:03 -0500

    add 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """class"""
+
+    def __init__(self, size=0):
+        """square class"""
+
+        self.size = size
+
+    def size(self):
+        """retrieve size from class"""
+
+        return self.__size
+
+    def size(self, value):
+        """square size setter """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """calculate the square area"""
+
+        return self.__size ** 2
