0ddb19723119acc76479be44a55c13b0c8c103d9
Duvan <duvanjarin@gmail.com>
2020-05-22 21:07:46 -0500

    add 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if type(size) is float and type(size) < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        print("#" * size)
