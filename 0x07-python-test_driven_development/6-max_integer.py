67cef5381e7e67b8bb909919162c051209f682c2
Duvan <duvanjarin@gmail.com>
2020-05-24 16:11:50 -0500

    add 6-max_integer.py

---
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to find the max integer in a list
+"""
+
+
+def max_integer(list=[]):
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
