e935dc1a1af631ae38afb34b82d883b2116cd693
Duvan <duvanjarin@gmail.com>
2020-05-25 18:59:08 -0500

    fix 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,36 +3,36 @@
 
 
 class Rectangle:
-    """class rectangle"""
+    """class: Rectangle"""
     def __init__(self, width=0, height=0):
-        """define class"""
-        self.width = width
-        self.height = height
+        """init class"""
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """define width"""
         return self.__width
 
-    @property
-    def height(self):
-        """define height"""
-        return self.__width
-
     @width.setter
     def width(self, value):
-        """define private class"""
+        """define pivate class"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """define height"""
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """define private class"""
+        """define pivate class"""
         if type(value) is not int:
-            raise TypeError("heigth must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("heigth must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
