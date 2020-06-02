0e0891c9f1841025f17bd15cacd869e75180306b
Duvan <duvanjarin@gmail.com>
2020-06-02 18:44:26 -0500

    add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def is_same_class(obj, a_class):
+    """function that compare if are the same"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
