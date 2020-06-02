a766deb1a0d6c96c300ae17407b8c43660d1705b
Duvan <duvanjarin@gmail.com>
2020-06-02 18:27:52 -0500

    add tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""return attributes of the class"""
+
+
+def lookup(obj):
+    """unction that returns the list of
+    available attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class MyList(list):
+    """print sorted list"""
+    def print_sorted(self):
+        print(sorted(self))
