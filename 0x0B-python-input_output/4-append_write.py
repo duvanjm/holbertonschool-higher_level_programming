b3767f1034aef6a1468d104fa4cbc2f6c5c5ef20
duvanjm <duvanjarin@gmail.com>
2020-06-03 17:32:20 -0500

    fix 4-append_write.py

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""append a string"""
+
+
 def append_write(filename="", text=""):
+    """append a string at the end of a text file"""
     with open(filename, "a", encoding="utf-8") as myFile:
         return myFile.write(text)
