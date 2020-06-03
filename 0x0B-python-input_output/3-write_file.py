d63a8a5e8a3ab079fedac088fb5ed99570a5ef2a
duvanjm <duvanjarin@gmail.com>
2020-06-03 17:30:49 -0500

    fix 3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""write on text file"""
+
+
 def write_file(filename="", text=""):
+    """write a string to a text file"""
     with open(filename, "w", encoding="utf-8") as myFile:
         return myFile.write(text)
