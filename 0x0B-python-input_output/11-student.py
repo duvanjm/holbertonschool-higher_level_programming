c0e8f73232f2075ef747f5bf5d008334ee98ac0a
duvanjm <duvanjarin@gmail.com>
2020-06-03 19:07:36 -0500

    fix 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        """class student"""
+        self.first_name = first_name
+        self.laste_name = last_name
+        self. age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
