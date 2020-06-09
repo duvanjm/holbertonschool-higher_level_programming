79ab4b857379c8ce9f0179fa65da6401b26fff0b
Duvan <duvanjarin@gmail.com>
2020-06-09 18:49:44 -0500

    add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """module"""
 
+import json
+
 
 class Base:
     """create a base class"""
@@ -13,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Dictionary to JSON string
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
