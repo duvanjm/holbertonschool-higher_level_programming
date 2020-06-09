3b9f53d9adb0e3d1dbded0c316320060d5b6f718
Duvan <duvanjarin@gmail.com>
2020-06-09 18:53:50 -0500

    fix base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Dictionary to JSON string
         """
