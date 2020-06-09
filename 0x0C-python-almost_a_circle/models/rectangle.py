aa37b9faa621cf995d5eefb46e4cd804adfbfbd8
Duvan <duvanjarin@gmail.com>
2020-06-09 12:06:18 -0500

    fix  rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ """
+    """class Rectanble based in class Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         self.width = width
