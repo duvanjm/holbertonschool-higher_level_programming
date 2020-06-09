ecbd8511937cb5d45d0a10ba4e02a08f0913f408
Duvan <duvanjarin@gmail.com>
2020-06-09 12:05:27 -0500

    add square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square based in class Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """f """
+        return ("[Square] {:d} {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                        self.y, self.width))
+
+    @property
+    def size(self):
+        """"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ """
+        self.width = value
+        self.height = value
