039ba3db774af112395da07f478a69e0f25ad764
Duvan <duvanjarin@gmail.com>
2020-06-09 17:16:13 -0500

    fix task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""module"""
+"""module
+"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Square based in class Rectangle"""
+    """class Square based in class Rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """Class constructor"""
+        """Class constructor
+        """
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        """str method"""
+        """str method
+        """
         return ("[Square] {:d} {:d}/{:d} - {:d}".format(self.id, self.x,
                                                         self.y, self.width))
 
     @property
     def size(self):
-        """ """
+        """getter of the size
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ """
+        """setter of the size
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ """
+        """update the class
+        """
         if len(args) < 0:
             for i in range(len(args)):
                 if i == 0:
