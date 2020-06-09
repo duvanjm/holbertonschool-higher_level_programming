4fd39c6fee565f1047999c561e35aa68bb62a141
Duvan <duvanjarin@gmail.com>
2020-06-09 18:50:40 -0500

    add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Dictionary to JSON string
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
