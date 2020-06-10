0b9d09a2d3c0a20f821c65ee28e6f84f5a9fecff
Duvan <duvanjarin@gmail.com>
2020-06-09 20:49:07 -0500

    add task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,4 +46,4 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return "[]"
         else:
-            return json.dumps(json_string)
+            return json.loads(json_string)
