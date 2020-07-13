52d6eb01da8337fee19391e329fd668b4b870ea1
duvanjm <duvanjarin@gmail.com>
2020-07-13 21:05:21 +0000

    fix 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table VALUES
-       (1, “John”, 10),
-       (2, “Alex”, 3),
-       (3, “Bob”, 14),
-       (4, “George”, 8);
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
