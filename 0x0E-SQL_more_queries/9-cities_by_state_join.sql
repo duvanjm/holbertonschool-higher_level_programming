e68326afe582498b8a04ff81e780d137b304039c
duvanjm <duvanjarin@gmail.com>
2020-07-14 21:04:14 +0000

    add 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.states_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.states_id = states.id;
