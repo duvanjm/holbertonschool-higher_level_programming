c3ff7b64f825e101fb4508e1f86a2ea304f61c78
Duvan <duvanjarin@gmail.com>
2020-08-18 17:02:03 -0500

    fix task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" """
+"""Find a peak """
 
 
 def find_peak(list_of_integers):
-    """ """
+    """function that finds a peak in a list of unsorted integers"""
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
