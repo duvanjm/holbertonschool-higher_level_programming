c77d9c155b7a572a795719cb17a3fb669d05c47c
Duvan <duvanjarin@gmail.com>
2020-08-24 11:57:02 -0500

    add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" """
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as responce:
+        html = responce.read()
+        print("Body responde:")
+        print("\t - type: {}".format(type(html)))
+        print("\t - content: {}".format(html))
+        print("\t - utf8 content: {}".format(html.decode("utf-8")))
