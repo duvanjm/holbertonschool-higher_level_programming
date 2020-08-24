90c89a4c65a3a36777690425401f9c05936d401a
Duvan <duvanjarin@gmail.com>
2020-08-24 12:06:57 -0500

    fix content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,6 @@ import urllib.request
 
 
 if __name__ == "__main__":
-
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as responce:
         html = responce.read()
         print("Body response:")
