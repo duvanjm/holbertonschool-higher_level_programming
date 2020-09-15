10990c07dfb499fd621560845d083978a2495d34
Duvan <duvanjarin@gmail.com>
2020-09-15 16:08:57 -0500

    fix semistandard task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (i = 0; i < array.length; i++) {
-  console.log(array[i])
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
