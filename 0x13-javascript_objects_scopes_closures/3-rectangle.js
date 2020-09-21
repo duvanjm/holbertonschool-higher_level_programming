fd496bc25fd070cd1153ba27c5ca11a5ea71496c
Duvan <duvanjarin@gmail.com>
2020-09-21 12:52:21 -0500

    fix indentation task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;    
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;    
     }
-    print() {
-      for (let i = 0; i < this.width; i++) {
-        console.log("X".repeat(this.width));
-      } 
-    }
+  }
+  print() {
+    for (let i = 0; i < this.width; i++) {
+    console.log("X".repeat(this.width));
+    } 
+  }
 }
 
 module.exports = Rectangle;
