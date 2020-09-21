736ca9db5c31c8f9e2939725215852c4a1a734ad
Duvan <duvanjarin@gmail.com>
2020-09-21 17:58:57 -0500

    fix task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   let rev = list.length - 1;
-  for (let i = 0; i < rev; i++) {
+  for (let i = 0; i < rev; i++, rev --) {
     const li = list[i];
     list[i] = list[rev];
     list[rev] = li;
