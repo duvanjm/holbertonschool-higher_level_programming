67694de276b6a47538aa127d3079915bb3d3fc62
Duvan <duvanjarin@gmail.com>
2020-09-22 14:57:49 -0500

    fix semistandard task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,11 +3,10 @@
 const arg = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(arg, 'utf-8', function(err, data) {
+fs.readFile(arg, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     console.log(data);
   }
 });
