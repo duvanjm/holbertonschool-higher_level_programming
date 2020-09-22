ac41a5adcae06aedc5dd3083d8b7de713c6d17fe
Duvan <duvanjarin@gmail.com>
2020-09-22 16:49:23 -0500

    add task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const request = require('request');
+
+request.get('https://swapi-api.hbtn.io/api/films/' + arg, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
