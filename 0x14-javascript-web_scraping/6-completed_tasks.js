54920c1b8fe1e5c13a67c42ab8fed77fb2f6d96d
Duvan <duvanjarin@gmail.com>
2020-09-22 19:32:23 -0500

    add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const request = require('request')
+const all = {};
+
+request.get(arg, function (error, responce, body) {
+  if (error) {
+    console.log(error)
+  } else {
+    const resul = JSON.parse(body);
+    for (const i of resul) {
+      if (i.completed === true) {
+        if (i.userId in all) {
+          all[i.userId]++;
+        } else {
+          all[i.userId] = 1;
+        }
+      }
+    }
+    console.log(all);
+  }
+});
