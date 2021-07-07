d6f1f249cd7a1086534d999b489c4c85fbc67031
duvanjm <duvanjarin@gmail.com>
2021-07-07 15:10:23 -0500

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
     name = sys.argv[1]
     pswd = sys.argv[2]
-    url = sys.argv[3]
+    url = 'https://api.github.com/user'
     r = requests.get(url, auth=(name, pswd)).json()
     try:
         print(r['id'])
